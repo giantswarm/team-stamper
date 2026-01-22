dc97f770729bfd459a470816e606cd5a131f3835
Łukasz Jakimczuk <lukasz.j@giantswarm.io>
2026-01-22 16:06:43 +0100

    Go fmt

--- internal/controller/predicate.go
+++ internal/controller/predicate.go
@@ -1,35 +1,35 @@
 package controller
 
 import (
-    "reflect"
+	"reflect"
 
-    v1 "k8s.io/api/core/v1"
-    "sigs.k8s.io/controller-runtime/pkg/event"
-    "sigs.k8s.io/controller-runtime/pkg/predicate"
+	v1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
 var ConfigMapDataChangedPredicate = predicate.Funcs{
-    CreateFunc: func(e event.CreateEvent) bool {
-        return true
-    },
-    DeleteFunc: func(e event.DeleteEvent) bool {
-        return false
-    },
-    UpdateFunc: func(e event.UpdateEvent) bool {
-        oldCm, ok := e.ObjectOld.(*v1.ConfigMap)
+	CreateFunc: func(e event.CreateEvent) bool {
+		return true
+	},
+	DeleteFunc: func(e event.DeleteEvent) bool {
+		return false
+	},
+	UpdateFunc: func(e event.UpdateEvent) bool {
+		oldCm, ok := e.ObjectOld.(*v1.ConfigMap)
 
-        if !ok {
-            return false
-        }
+		if !ok {
+			return false
+		}
 
-        newCm, ok := e.ObjectNew.(*v1.ConfigMap)
-        if !ok {
-            return false
-        }
+		newCm, ok := e.ObjectNew.(*v1.ConfigMap)
+		if !ok {
+			return false
+		}
 
-        return !reflect.DeepEqual(oldCm.Data, newCm.Data)
-    },
-    GenericFunc: func(e event.GenericEvent) bool {
-        return false
-    },
+		return !reflect.DeepEqual(oldCm.Data, newCm.Data)
+	},
+	GenericFunc: func(e event.GenericEvent) bool {
+		return false
+	},
 }

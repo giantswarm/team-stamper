609ef235ee038c11d1e81357cd8827bf9e9f7046
Łukasz Jakimczuk <lukasz.j@giantswarm.io>
2026-01-23 23:09:01 +0100

    Formatting

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -1,31 +1,31 @@
 package integration
 
 import (
-    "fmt"
-    "context"
+	"context"
+	"fmt"
 
-    . "github.com/onsi/ginkgo/v2"
-    . "github.com/onsi/gomega"
-    "k8s.io/apimachinery/pkg/types"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/types"
 
-    helmv2 "github.com/fluxcd/helm-controller/api/v2"
+	helmv2 "github.com/fluxcd/helm-controller/api/v2"
 )
 
 var _ = Describe("Integration Tests", func() {
 
-    ctx := context.Background()
+	ctx := context.Background()
 
-    Context("When team mapping is available for an app and this app gets installed with HelmRelease", func() {
-        It("should a the team annotation", func() {
-            target := types.NamespacedName{
-                Name:      "myapp-1",
-                Namespace: "default",
-            }
+	Context("When team mapping is available for an app and this app gets installed with HelmRelease", func() {
+		It("should a the team annotation", func() {
+			target := types.NamespacedName{
+				Name:      "myapp-1",
+				Namespace: "default",
+			}
 
-            hrcr := helmv2.HelmRelease{}
-            err := k8sClient.Get(ctx, target, &hrcr)
+			hrcr := helmv2.HelmRelease{}
+			err := k8sClient.Get(ctx, target, &hrcr)
 
-            Expect(err).ToNot(HaveOccurred())
-        })
+			Expect(err).ToNot(HaveOccurred())
+		})
 	})
 })
--- tests/integration/suite_test.go
+++ tests/integration/suite_test.go
@@ -3,52 +3,52 @@ package integration
 import (
 	"testing"
 
-    . "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-    "k8s.io/apimachinery/pkg/runtime"
-    "k8s.io/client-go/tools/clientcmd"
-    clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-    "sigs.k8s.io/controller-runtime/pkg/client"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-    helmv2 "github.com/fluxcd/helm-controller/api/v2"
+	helmv2 "github.com/fluxcd/helm-controller/api/v2"
 	sourcev1beta2 "github.com/fluxcd/source-controller/api/v1beta2"
 )
 
 const (
-    // this gets created in config/setup.sh
-    kindKubeConfig = "/tmp/kind.kubeconfig"
+	// this gets created in config/setup.sh
+	kindKubeConfig = "/tmp/kind.kubeconfig"
 )
 
 var (
-    k8sClient client.Client
+	k8sClient client.Client
 )
 
 func TestIntegration(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-    RunSpecs(t, "Integration Suite")
+	RunSpecs(t, "Integration Suite")
 }
 
 var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
-    config, err := clientcmd.BuildConfigFromFlags("", kindKubeConfig)
-    Expect(err).ToNot(HaveOccurred())
+	config, err := clientcmd.BuildConfigFromFlags("", kindKubeConfig)
+	Expect(err).ToNot(HaveOccurred())
 
-    scheme := runtime.NewScheme()
+	scheme := runtime.NewScheme()
 
-    err = clientgoscheme.AddToScheme(scheme)
-    Expect(err).ToNot(HaveOccurred())
+	err = clientgoscheme.AddToScheme(scheme)
+	Expect(err).ToNot(HaveOccurred())
 
-    err = helmv2.AddToScheme(scheme)
-    Expect(err).ToNot(HaveOccurred())
+	err = helmv2.AddToScheme(scheme)
+	Expect(err).ToNot(HaveOccurred())
 
-    err = sourcev1beta2.AddToScheme(scheme)
-    Expect(err).ToNot(HaveOccurred())
+	err = sourcev1beta2.AddToScheme(scheme)
+	Expect(err).ToNot(HaveOccurred())
 
-    k8sClient, err = client.New(config, client.Options{Scheme: scheme})
-    Expect(err).ToNot(HaveOccurred())
+	k8sClient, err = client.New(config, client.Options{Scheme: scheme})
+	Expect(err).ToNot(HaveOccurred())
 })
